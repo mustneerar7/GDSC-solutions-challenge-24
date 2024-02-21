1ccb5fa7637ce13b0d5ff5fc5ce2de528ce6481c
hamna <hamnajabbar36@gmail.com>
2024-02-21 17:58:38 +0500

    ProductDetails

--- navigators/LoginStack.js
+++ navigators/LoginStack.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Exchange } from "../screens/Exchange/Exchange";
 import { Onboard } from "../screens/Onboard/Onboard";
 import { Login } from "../screens/Login/Login";
-import { ProductDetails } from "../screens/ProductDetails/ProductDetails";
+// import { ProductDetails } from "../screens/ProductDetails/ProductDetails";
 // import {Complete} from "../screens/Complete/Complete"
 // Bottom Tabs component.
 import { BottomTabs } from "./BottomTabs";
@@ -17,7 +17,7 @@ const LoginStack = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       {/* <Stack.Screen name="Onboard" component={Onboard} /> */}
       {/* <Stack.Screen name="Complete" component={Complete} /> */}
-      <Stack.Screen name="ProductDetails" component={ProductDetails} />
+      {/* <Stack.Screen name="ProductDetails" component={ProductDetails} /> */}
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Choice" component={Choice}/>
       <Stack.Screen name="BottomTabs" component={BottomTabs} />
